cf2cf75e4491850cc31bf0c9019466f7bb2c0f1e
HariTripurari <eswartripurari@gmail.com>
2025-04-03 13:36:53 -0500

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,15 @@
+use master;
+
+--creating database
+create database Datawarehouse_project;
+
+use Datawarehouse_project;
+
+
+--creating schemas for bronze, silver, and gold
+create schema bronze;
+GO
+create schema silver;
+GO
+create schema gold;
+GO
